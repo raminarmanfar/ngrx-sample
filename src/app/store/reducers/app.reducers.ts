bff0882f2bc3682205b3066236af2b51e03006ae
raminarman <ramin.armanfar@msg.group>
2019-11-23 16:43:56 +0100

    test commit

--- src/app/store/reducers/app.reducers.ts
+++ src/app/store/reducers/app.reducers.ts
@@ -1,8 +1,8 @@
 import { ActionReducerMap } from '@ngrx/store';
 import { routerReducer } from '@ngrx/router-store';
+import { IAppState } from '../state/app.state';
 import { configReducers } from './config.reducers';
 import { userReducers } from './user.reducers';
-import { IAppState } from '../state/app.state';
 
 export const appReducers: ActionReducerMap<IAppState, any> = {
     router: routerReducer,
